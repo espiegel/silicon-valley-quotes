e5de0724e8a34af7e391519ee3381d7aa1901925
Eidan <eidan@sleeprate.com>
2015-09-23 16:05:51 +0300

    added some css

--- public/javascripts/quotes.js
+++ public/javascripts/quotes.js
@@ -9,27 +9,54 @@ var quotes = (function () {
         );
     };
 
+    quotes.updateQuoteWithData = function(data) {
+        var container = $(".quoteContainer");
+
+        container.empty();
+
+        var paragraph = $("<p>", {
+            text: data.quote,
+            "class": "quoteText"
+        });
+        container.append(paragraph);
+
+        var imgContainer = $("<div>", {
+            "class": "quoteImageContainer"
+        });
+
+        var imgTitle = $("<p>", {
+            text: data.name,
+           "class": "quoteImageTitle"
+        });
+
+        var img = $("<img>", {
+            src: "/assets/images/"+data.name.toLowerCase()+".jpg",
+            title: data.name,
+            "class": "quoteImage"
+        });
+        img.attr('alt', data.name);
+
+        imgContainer.append(imgTitle);
+        imgContainer.append(img);
+        container.append(imgContainer);
+    };
+
+    quotes.generateQuote = function() {
+        quotes.getRandomQuote(function(data) {
+            quotes.updateQuoteWithData(data);
+        });
+    };
+
     return quotes;
 })();
 
 (function () {
     $(document).ready(function () {
-        $('#btnQuote').click(function () {
-            quotes.getRandomQuote(function (data) {
-                var container = $("#quoteContainer");
 
-                container.empty();
+        quotes.generateQuote();
 
-                var paragraph = $("<p>", {
-                    text: data.quote
-                });
-                container.append(paragraph);
-
-                var img = $("<img>", {
-                    src: "/assets/images/"+data.name.toLowerCase()+".jpg"
-                });
-                container.append(img);
-            })
+        $('.btnQuote').click(function () {
+            quotes.generateQuote();
         })
     })
 })
